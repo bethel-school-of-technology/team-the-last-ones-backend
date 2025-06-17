6d573511d650f04aae9924562b25e44f9d1f7766
Samuel Shivy <sshivy7@gmail.com>
2025-06-16 22:57:31 -0400

    feat(database): add initial database migration

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
+using final_project.Migrations;
+using final_project.Repositories;
 using Microsoft.AspNetCore.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddControllers();
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Unable to load connection string");
+builder.Services.AddDbContext<AruchaDb>(options => {
+    options.UseSqlite(connectionString);
+});
+
+// try {
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+// } catch {
+//     throw new Exception("Unable to register user repository");
+// }
 
 var app = builder.Build();
 
@@ -14,5 +31,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
