ce14e34cf42390130ebcdc1cb532928274bd1fc1
Samuel Shivy <sshivy7@gmail.com>
2025-06-23 18:36:23 -0400

    checkpoint: add Recipe constructor and Db context

--- Repositories/RecipeRepository.cs
+++ Repositories/RecipeRepository.cs
@@ -1,9 +1,17 @@
+using final_project.Migrations;
 using final_project.Models;
 
 namespace final_project.Repositories;
 
 public class RecipeRepository: IRecipeRepository
 {
+    private readonly AruchaDb _context;
+
+    public RecipeRepository(AruchaDb context) 
+    {
+        _context = context;
+    }
+
     public Recipe? GetRecipeByUserId(int userId) 
     {
         throw new NotImplementedException();
