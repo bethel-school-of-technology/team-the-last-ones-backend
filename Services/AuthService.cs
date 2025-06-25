2f98b6a8c464b31100f29e48a69850213cbc3eed
Samuel Shivy <sshivy7@gmail.com>
2025-06-24 23:17:09 -0400

    wip: add AuthService outline

---
+++ Services/AuthService.cs
@@ -0,0 +1,14 @@
+using final_project.Models;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace final_project.Services;
+
+public class AuthService: IAuthService {
+    public User RegisterUser(User user) {
+        throw new NotImplementedException();
+    }
+
+    public string Login(string email, string password) {
+        throw new NotImplementedException();
+    }
+}
