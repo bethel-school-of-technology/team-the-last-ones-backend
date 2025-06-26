060a3e8fa7a9525b488d3d091b773679fa58992d
Samuel Shivy <sshivy7@gmail.com>
2025-06-25 20:40:48 -0400

    wip: add AuthService

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,4 +1,8 @@
 using Bcrypt = BCrypt.Net.BCrypt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 using final_project.Models;
 using final_project.Repositories;
@@ -7,9 +11,11 @@ namespace final_project.Services;
 
 public class AuthService: IAuthService {
     private IUserRepository _userRepo;
+    private IConfiguration _config;
 
-    public AuthService(IUserRepository userRepository) {
+    public AuthService(IUserRepository userRepository, IConfiguration config) {
         _userRepo = userRepository;
+        _config = config;
     }
 
     public User RegisterUser(User user) {
@@ -21,6 +27,43 @@ public class AuthService: IAuthService {
     }
 
     public string Login(string email, string password) {
-        throw new NotImplementedException();
+        // throw new NotImplementedException();
+        User? user = _userRepo.GetUserByEmail(email);
+        bool verified = false;
+
+        if (user != null) {
+            verified = Bcrypt.Verify(password, user.Password);
+        }
+
+        if (user == null) {
+            return String.Empty;
+        }
+
+        return BuildToken(user);
+    }
+
+    private string BuildToken(User user) {
+        string secret = _config.GetValue<String>("TokenSecret") ?? throw new Exception("Failed to load JWT secret!");
+        var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+
+        var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new Claim[] {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserId.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.PreferredUsername, user.UserName)
+        };
+
+        SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = signingCredentials
+        };
+
+        JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+        SecurityToken token = handler.CreateToken(descriptor);
+        string encodedToken = handler.WriteToken(token);
+
+        return encodedToken;
     }
 }
