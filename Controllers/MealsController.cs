26b4b8e5f934a5c539a50f93ea63becfeb766321
David Koehn <davidnkoehn@yahoo.com>
2025-06-26 13:11:44 -0500

    add the conTroller for mealsPlan

---
+++ Controllers/MealsController.cs
@@ -0,0 +1,12 @@
+using final_project.Models;
+using final_project.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace final_project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class mealsController : Controller
+    {
+    }
+}
